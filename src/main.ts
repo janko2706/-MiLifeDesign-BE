ab57005cda10f09f1b515ddeaff2f8551e909d66
Jan Bogovic <1087894@ucn.dk>
2022-11-20 01:11:04 +0100

    added error handling config

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { PrismaModule } from './app.module';
+import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(PrismaModule);
 
   	// 👇 binds ValidationPipe to the entire application
     app.useGlobalPipes(new ValidationPipe());
+    // 👇 apply PrismaClientExceptionFilter to entire application, requires HttpAdapterHost because it extends BaseExceptionFilter
+    const { httpAdapter } = app.get(HttpAdapterHost);
+    app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   const config = new DocumentBuilder()
     .setTitle('Mi Life Design - Back End')
